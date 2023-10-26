846e6b3ba026f6d685965d57c7668a042830dae7
Navid <navidahmadi6293@gmail.com>
2023-10-26 21:57:21 +0330

    update

--- order_module/views.py
+++ order_module/views.py
@@ -12,20 +12,20 @@ import json
 # Create your views here.
 
 # ? sandbox merchant
-if settings.SANDBOX:
-    sandbox = 'sandbox'
-else:
-    sandbox = 'www'
-
-ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
-ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
-ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
-
-amount = 1000  # Rial / Required
-description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
-phone = 'YOUR_PHONE_NUMBER'  # Optional
-# Important: need to edit for realy server.
-CallbackURL = 'http://127.0.0.1:8080/order/verify-payment/'
+# if settings.SANDBOX:
+#     sandbox = 'sandbox'
+# else:
+#     sandbox = 'www'
+#
+# ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+# ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+# ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+#
+# amount = 1000  # Rial / Required
+# description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+# phone = 'YOUR_PHONE_NUMBER'  # Optional
+# # Important: need to edit for realy server.
+# CallbackURL = 'http://127.0.0.1:8080/order/verify-payment/'
 
 
 def add_product_to_order(request: HttpRequest):
@@ -73,65 +73,65 @@ def add_product_to_order(request: HttpRequest):
             'icon': 'error'
         })
 
-
-@login_required
-def request_payment(request: HttpRequest):
-    current_order, created = Order.objects.get_or_create(is_paid=False, user_id=request.user.id)
-    total_price = current_order.calculate_total_price()
-
-    if total_price == 0:
-        return redirect(reverse('user_basket_page'))
-    data = {
-        "MerchantID": settings.MERCHANT,
-        "Amount": total_price * 10,
-        "Description": description,
-        "Phone": phone,
-        "CallbackURL": CallbackURL,
-    }
-    data = json.dumps(data)
-    # set content length by data
-    headers = {'content-type': 'application/json', 'content-length': str(len(data))}
-    try:
-        response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
-
-        if response.status_code == 200:
-            response = response.json()
-            if response['Status'] == 100:
-                current_order.is_paid = True
-                current_order.payment_date = time.time()
-                current_order.save()
-                return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),
-                        'authority': response['Authority']}
-            else:
-                return {'status': False, 'code': str(response['Status'])}
-        return response
-
-    except requests.exceptions.Timeout:
-        return {'status': False, 'code': 'timeout'}
-    except requests.exceptions.ConnectionError:
-        return {'status': False, 'code': 'connection error'}
-
-
-@login_required
-def verify_payment(request: HttpRequest):
-    current_order, created = Order.objects.get_or_create(is_paid=False, user_id=request.user.id)
-    total_price = current_order.calculate_total_price()
-
-    authority = request.GET['Authority']
-    data = {
-        "MerchantID": settings.MERCHANT,
-        "Amount": total_price * 10,
-        "Authority": authority,
-    }
-    data = json.dumps(data)
-    # set content length by data
-    headers = {'content-type': 'application/json', 'content-length': str(len(data))}
-    response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
-
-    if response.status_code == 200:
-        response = response.json()
-        if response['Status'] == 100:
-            return {'status': True, 'RefID': response['RefID']}
-        else:
-            return {'status': False, 'code': str(response['Status'])}
-    return response
+#
+# @login_required
+# def request_payment(request: HttpRequest):
+#     current_order, created = Order.objects.get_or_create(is_paid=False, user_id=request.user.id)
+#     total_price = current_order.calculate_total_price()
+#
+#     if total_price == 0:
+#         return redirect(reverse('user_basket_page'))
+#     data = {
+#         "MerchantID": settings.MERCHANT,
+#         "Amount": total_price * 10,
+#         "Description": description,
+#         "Phone": phone,
+#         "CallbackURL": CallbackURL,
+#     }
+#     data = json.dumps(data)
+#     # set content length by data
+#     headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+#     try:
+#         response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+#
+#         if response.status_code == 200:
+#             response = response.json()
+#             if response['Status'] == 100:
+#                 current_order.is_paid = True
+#                 current_order.payment_date = time.time()
+#                 current_order.save()
+#                 return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),
+#                         'authority': response['Authority']}
+#             else:
+#                 return {'status': False, 'code': str(response['Status'])}
+#         return response
+#
+#     except requests.exceptions.Timeout:
+#         return {'status': False, 'code': 'timeout'}
+#     except requests.exceptions.ConnectionError:
+#         return {'status': False, 'code': 'connection error'}
+#
+#
+# @login_required
+# def verify_payment(request: HttpRequest):
+#     current_order, created = Order.objects.get_or_create(is_paid=False, user_id=request.user.id)
+#     total_price = current_order.calculate_total_price()
+#
+#     authority = request.GET['Authority']
+#     data = {
+#         "MerchantID": settings.MERCHANT,
+#         "Amount": total_price * 10,
+#         "Authority": authority,
+#     }
+#     data = json.dumps(data)
+#     # set content length by data
+#     headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+#     response = requests.post(ZP_API_VERIFY, data=data, headers=headers)
+#
+#     if response.status_code == 200:
+#         response = response.json()
+#         if response['Status'] == 100:
+#             return {'status': True, 'RefID': response['RefID']}
+#         else:
+#             return {'status': False, 'code': str(response['Status'])}
+#     return response
